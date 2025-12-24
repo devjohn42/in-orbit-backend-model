348573f27edeb2ed5cc5118c62cc8525f9123fee
devjohn42 <devjohncb@gmail.com>
2025-12-24 11:24:05 -0300

    :sparkles: feat: add cuid2 as if format

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,10 @@
 import { timestamp, integer, text, pgTable } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$default(() => createId()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frenquency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
@@ -10,7 +13,9 @@ export const goals = pgTable('goals', {
 })
 
 export const goalCompletios = pgTable('goal_completions', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$default(() => createId()),
   goalId: text('gold_id')
     .references(() => goals.id)
     .notNull(),
