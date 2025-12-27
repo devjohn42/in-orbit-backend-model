ee69ea16662fb3e27028629639e270f36fa69669
devjohn42 <devjohncb@gmail.com>
2025-12-26 23:27:17 -0300

    :sparkles: feat(route): creates the create goal route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import z from 'zod'
+import { createGoal } from '../use-cases/create-goal'
 
 const app = fastify()
+
+app.post('/goals', async (request) => {
+  const createGoalBodySchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalBodySchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
---
+++ src/use-cases/create-goal.ts
@@ -0,0 +1,26 @@
+import { db } from '../db'
+import { goals } from '../db/schema'
+
+interface CreateGoalRequest {
+  title: string
+  desiredWeeklyFrequency: number
+}
+
+export const createGoal = async ({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) => {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning()
+
+  const goal = result[0]
+
+  return {
+    goal,
+  }
+}
