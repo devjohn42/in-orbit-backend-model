f255e8c0d6572de0446ce6ad39a4fcc77e2324c6
devjohn42 <devjohncb@gmail.com>
2026-01-08 01:37:03 -0300

    :bug: fix: sort the days of the week in descending order

--- src/use-cases/get-week-summary.ts
+++ src/use-cases/get-week-summary.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { and, eq, gte, lte, sql } from 'drizzle-orm'
+import { and, desc, eq, gte, lte, sql } from 'drizzle-orm'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 
@@ -55,6 +55,7 @@ export const getWeekSummary = async () => {
 			})
 			.from(goalsCompletedInWeek)
 			.groupBy(goalsCompletedInWeek.completedAtDate)
+			.orderBy(desc(goalsCompletedInWeek.completedAtDate))
 	)
 
 	type GoalsPerDay = Record<
